655702f67203e4a81aeef41ee8235e9650c54fc2
Your name <kiril.kip@gmail.com>
2023-03-13 23:44:48 +0200

    fourth function

--- homework03/script.js
+++ homework03/script.js
@@ -13,7 +13,17 @@ function capitalLetter(str) {
     return result;
 }
 
-function deleteLetters (char, str) {
+function deleteLetters(char, str) {
     let newStr = str.split(char).join("");
     return newStr;
+}
+
+function countLetters(char, str) {
+    let count = 0
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            count = count + 1;
+        }
+    }
+    return count;
 }
