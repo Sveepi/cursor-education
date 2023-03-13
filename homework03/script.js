3463bbe21fa178dd774b67611677db3dfc75f4fb
Your name <kiril.kip@gmail.com>
2023-03-13 23:35:02 +0200

    second function

--- homework03/script.js
+++ homework03/script.js
@@ -5,3 +5,11 @@ function getRandomNumber(n, m) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+function capitalLetter(str) {
+    let uper = str.charAt(0).toUpperCase();
+    let lower = str.slice(1).toLowerCase();
+    let result = uper + lower;
+
+    return result;
+}
+
